20c1744b442eaf5c9e56031cfde3a9af075b9f22
Omkar Dubas <chandandubas@gmail.com>
2016-09-03 23:36:01 +0530

    added student class

---
+++ scripts/classes/student.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Omkar Dubas
+ * Date: 03-09-2016
+ * Time: 23:23
+ */
+
+require_once('../config.php');
+require_once('../sql/shared/ez_sql_core.php');
+require_once('../sql/mysqli/ez_sql_mysqli.php');
+require_once('college.php');
+
+
+class Student extends Comments
+{
+    protected $rating = null;
+
+    public function give($rating){
+        $db = new ezSQL_mysqli(DB_USER, DB_PASS, DB_NAME, DB_HOST);
+        $query = "INSERT INTO `student`(`$rating`, `$this->comments`)";
+
+        if($db->query($query)){
+            $this->rating = $rating;
+        }else{
+            throw new Exception("Failed to provide Ratings. Database Error");
+        }
+    }
+
+    public  function rmv($rating){
+        $db = new ezSQL_mysqli(DB_USER, DB_PASS, DB_NAME, DB_HOST);
+        $query = "DELETE FROM `Student` WHERE (`rating = $rating`)";
+
+        if($db->$query($query)){
+
+        }else
+        {
+            throw new Exception("Failed to remove Ratings. Database Error");
+        }
+    }
+}
