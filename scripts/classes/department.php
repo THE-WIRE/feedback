a223a29b3cf3ce46725f86152958258988b6f7d0
itssuyog96 <itssuyog96@gmail.com>
2016-09-03 16:32:40 +0530

    Department class created Added css files

--- scripts/classes/department.php
+++ scripts/classes/department.php
@@ -6,17 +6,40 @@
  * Date: 03-Sep-16
  * Time: 3:00 PM
  */
+
+require_once('../config.php');
+require_once('sql/shared/ez_sql_core.php');
+require_once('sql/mysqli/ez_sql_mysqli.php');
+
 class Department
 {
+    private $dept_id    = '';
+    private $dept_name  = '';
 
-        public function add(){
-            
-            include('../connection.php');
-            $db->get_results('SELECT * FROM `department`');
-            $db->debug();
+    public function add($dept_id, $dept_name, $col_id){
+
+        $db = new ezSQL_mysqli(DB_USER, DB_PASS, DB_NAME, DB_HOST);
+        $query = "INSERT INTO `department`(`dep_id`, `dep_name`, `colg_id`) VALUES('$dept_id', '$dept_name', '$col_id')";
+
+        if($db->query($query)){
+            $this->dept_id   = $dept_id;
+            $this->dept_name = $dept_name;
+        }else{
+            throw new Exception('Failed adding new department. Database Error.');
         }
 
-}
+    }
 
-$d = new Department();
-$d->add();
+    public function delete($dept_id, $col_id){
+
+        $db = new ezSQL_mysqli(DB_USER, DB_PASS, DB_NAME, DB_HOST);
+        $query = "DELETE FROM `department` WHERE `colg_id` = '$col_id' AND `dep_id` = '$dept_id'";
+
+        if($db->query($query)){
+
+        }else{
+            throw new Exception('Failed deleting department. Database Error.');
+        }
+    }
+
+}
